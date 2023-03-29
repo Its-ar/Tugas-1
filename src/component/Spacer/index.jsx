bb0add537e0377b0591b785b07584bf64386f53f
armaneffendy003@gmail.com <armaneffendy003@gmail.com>
2023-03-29 17:25:02 +0700

    update

--- src/component/Spacer/index.jsx
+++ src/component/Spacer/index.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 
-/**
- * Spacer component
- * @param {string} children - children of the component
- * @returns {JSX.Element} - spacer component
- */
 const Spacer = ({ children }) => {
   return <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>{children}</div>;
 };
--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className={styles.row}>
           <div className={styles.column1}>
             <h3>Edspert</h3>
-            <p>Jl. Watugede No.58, Wonorejo, Sariharjo, Kec. Ngaglik, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55581 +6282123824483 halo@widyaedu.com</p>
+            <p>Jl Rukun Ujung,Pejaten Timur,Pasar Minggu, KOTA JAKARTA SELATAN,DKI JAKARTA,ID 12520</p>
           </div>
           <div className={styles.column2}>
             <div className={styles.row}>
@@ -15,13 +15,13 @@ const Footer = () => {
                 <h3>Program</h3>
                 <ul className={styles.column2List}>
                   <li>
-                    <a href="https://edspert.id/#">Program 1</a>
+                    <a href="https://edspert.id/#">Online Course</a>
                   </li>
                   <li>
-                    <a href="https://edspert.id/#">Program 2</a>
+                    <a href="https://edspert.id/#">Mini bootcamp</a>
                   </li>
                   <li>
-                    <a href="https://edspert.id/#">Program 3</a>
+                    <a href="https://edspert.id/#">bootcamp</a>
                   </li>
                 </ul>
               </div>
@@ -56,7 +56,7 @@ const Footer = () => {
             </div>
             <div className={styles.row}>
               <div className={styles.column2}>
-                <p>&copy; 2023 Rosdyana Kusuma. All Rights Reserved.</p>
+                <p>&copy; Arman Ghazali.</p>
               </div>
             </div>
           </div>
